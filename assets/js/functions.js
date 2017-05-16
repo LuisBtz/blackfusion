8f945541c886292111e688bc0688a90121cd5ec4
Luis Benitez <luisbttf@gmail.com>
2017-05-15 22:36:23 -0500

    home

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
 
-  // Get started!
-
+    $( "section.pre" ).delay(5000).fadeOut(1000);
+    $( ".container" ).delay(6000).fadeIn(1500);
 });
+
+  document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.element', {
+        strings: ["Emprender su propio negocio", "Tener un horario flexible", "Obtener ingresos extras", "Ser su propio jefe", "Cumplir sus sueños"],
+        typeSpeed: 0
+      });
+  });
